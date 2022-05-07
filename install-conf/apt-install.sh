f5d5e640e65f7aadc06daf3a1f03f815dda2e42a
Meng-Huan Yu <2252236+cebrusfs@users.noreply.github.com>
2022-05-07 19:28:07 +0800

    dotfile: update default install

---
+++ install-conf/apt-install.sh
@@ -0,0 +1,29 @@
+# vim: syntax=sh:
+
+set -e
+
+BASEDIR="$(dirname "${BASH_SOURCE[0]}")"
+source "${BASEDIR}/utils.sh"
+
+function homebrew_install() {
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+    brew analytics off
+}
+
+function install_pkgs() {
+    sudo apt install -y ripgrep fd-find
+}
+
+function main() {
+    if ! is_linux; then
+        exit 0
+    fi
+
+    if ! check_command apt; then
+        exit 0
+    fi
+    install_pkgs
+}
+
+main
